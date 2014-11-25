e8a26864595c342e8b59c700cb7a6bd9c007ad46
Shiva Srivastava <shivasrivastava@Shivas-MacBook-Pro.local>
2014-11-24 20:29:02 -0600

    Sending batch item tested

--- cpp_zhtclient.cpp
+++ cpp_zhtclient.cpp
@@ -342,7 +342,7 @@ int ZHTClient::addToBatch(Request item, ZPack &batch){
 	newItem->set_val(item.val);
 	newItem->set_client_ip(item.client_ip);
 	newItem->set_client_port(item.client_port);
-	newItem->set_opcode(item.opcode);
+//	newItem->set_opcode(item.opcode);
 	newItem->set_max_wait_time(item.max_wait_time);
 	newItem->set_consistency(item.consistency);
 	return 0;
